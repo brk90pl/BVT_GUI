e9ff5082d69f770edd0e5b8b01be9ce987973e34
Bartek <61869051+brk90pl@users.noreply.github.com>
2022-01-12 15:51:42 +0100

    Add files via upload

--- bots_app.py
+++ bots_app.py
@@ -6,17 +6,19 @@ from multiapp import MultiApp
 #(2)then a new line for your page "app.add_app("YOURPAGE", YOURPAGE.app)"
 #(3)copy existing snail.py in apps and rename it to YOURPAGE.py
 #(4)Update YOURPAGE.py to reference your bot folder
-from apps import home,snail,market,scalper,reinvest
+from apps import home,str01,str02,str03,str04,snail
 
-st.set_page_config(page_title='JimBot BVT Bots', page_icon='\xe2\x9c\x85', layout='wide')
+st.set_page_config(page_title='BVT Bots', page_icon='\xe2\x9c\x85', layout='wide')
 
 app = MultiApp()
 
 # Add all your application here
 app.add_app("Home", home.app)
-app.add_app("Snail", snail.app)
-#app.add_app("Scalper", scalper.app)
-app.add_app("Market", market.app)
+app.add_app("01", str01.app)
+app.add_app("02", str02.app)
+app.add_app("03", str03.app)
+app.add_app("04", str04.app)
+#app.add_app("Market", market.app)
 #app.add_app("Reinvest", reinvest.app)
 
 
